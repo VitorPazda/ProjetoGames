937f95ec770a45caa63c4c0935785fafef52ef36
Nogrilo <nogriloesirjeferson@gmail.com>
2026-05-09 11:13:16 -0300

    add a classe Games, com seus atributos

---
+++ api/src/models/Games.js
@@ -0,0 +1,37 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
+
+const Games = sequelize.define('Games', {
+    idGames: {
+        type:   DataTypes.INTEGER,
+        primaryKey:     true,
+        autoIncrement: true,
+    },
+
+    title: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+
+    description: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+
+    genre: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+
+    releaseDate: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+
+    image_url: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+});
+
+module.exports = Games;
