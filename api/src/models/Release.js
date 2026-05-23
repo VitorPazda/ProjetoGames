9d37fcfe6555f013a39249235221909aca1a9d21
Nogrilo <nogriloesirjeferson@gmail.com>
2026-05-22 22:53:06 -0300

    alterando o id

--- api/src/models/Release.js
+++ api/src/models/Release.js
@@ -2,7 +2,7 @@ const { DataTypes } = require('sequelize');
 const sequelize = require('../config/database');
 
 const Release = sequelize.define('Releases', {
-    idRelease: {
+    id: {
         type:   DataTypes.INTEGER,
         primaryKey:     true,
         autoIncrement: true,
